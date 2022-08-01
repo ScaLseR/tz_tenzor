741efbc5a31a307fcf55fe5f6cf33435e27b7d3d
scalser <scalserstv@yandex.ru>
2022-08-01 12:47:42 +0300

    added requirements

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,4 @@ class BasePageLocators:
 class MainPageLocators:
     """locators for MainPage"""
     FIND_FIELD = (By.ID, 'text')
+    SUGGEST = (By.CSS_SELECTOR, '.mini-suggest__popup.mini-suggest__popup_svg_yes.mini-suggest__popup_theme_tile')
--- test_page.py
+++ test_page.py
@@ -1,6 +1,7 @@
 """Test Cases Module"""
 from pages.main_page import MainPage
 from pages.locators import MainPageLocators
+from time import sleep
 
 _URL = 'http://yandex.ru'
 _TEXT = 'Тензор'
@@ -12,3 +13,5 @@ def test_search_in_ya(browser):
     page.open()
     assert page.is_element_present(*MainPageLocators.FIND_FIELD), 'Отсутствует поле для поиска!'
     page.input_text_to_element(*MainPageLocators.FIND_FIELD, _TEXT)
+    assert page.is_element_present(*MainPageLocators.SUGGEST), 'Отсутствует таблица с подсказками!'
+    sleep(10)
